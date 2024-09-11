34606fe217d2d3a8d9af5e4ea513472e0a852bb4
Ian Vieira <ianmarques619@gmail.com>
2024-09-10 21:02:48 -0300

    criando os testes de validação

--- src/assets/components/input.tsx
+++ src/assets/components/input.tsx
@@ -1,5 +1,5 @@
 import { BoxInput, InputStyled } from './input.styles';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
 import { ButtonEye } from './button.styles';
 
@@ -15,8 +15,33 @@ interface InputProps {
 
 const InputComponent = ({ icon, type = 'text', placeholder, margin, user, ...props }: InputProps) => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [userValue, setUserValue] = useState(''); 
+  const [passwordValue, setPasswordValue] = useState('');  
+  const [isFormValid, setIsFormValid] = useState(false);
+  
+  const validateForm = () => {
+    if (userValue.length > 3 && passwordValue.length > 6) {
+      setIsFormValid(true);
+    } else {
+      setIsFormValid(false);
+    }
+  };
+
+  useEffect(() => {
+    validateForm();
+  }, [userValue, passwordValue]);
+
+  const handleSubmit = () => {
+    if (isFormValid) {
+      console.log('Enviando dados:', { userValue, passwordValue });
+      alert('Dados enviados com sucesso!');
+    } else {
+      alert('Por favor, preencha corretamente o formulário.');
+    }
+  };
 
   const inputType = type === 'password' ? (isPasswordVisible ? 'text' : 'password') : type;
+
 
   return (
     <BoxInput margin={margin}>
@@ -26,6 +51,9 @@ const InputComponent = ({ icon, type = 'text', placeholder, margin, user, ...pro
         {...props}
         prefix={icon}
         type={inputType}  
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          type === 'text' ? setUserValue(e.target.value) : setPasswordValue(e.target.value)
+        }
       />
       {type === 'password' && (
      
