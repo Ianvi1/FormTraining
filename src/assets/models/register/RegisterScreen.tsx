8eaded091a85d0a14d8fce43661886287ac53573
Ian Vieira <ianmarques619@gmail.com>
2024-09-30 21:50:29 -0300

    adicionando input de email no register e ajustando infos necessárias sobre ele

--- src/assets/models/register/RegisterScreen.tsx
+++ src/assets/models/register/RegisterScreen.tsx
@@ -1,17 +1,76 @@
-import { ContainerLoginScreen, ContainerLogoScreen, InputAreaLoginScreen, TextLeftSide, TitleLoginScreen } from '../../styles/RegisterScreen.styles'
- 
+import { useState } from "react";
+import { ButtonComponent } from "../../components/button";
+import InputComponent from "../../components/input";
+import SVGlogo from "../../styles/icons/logo";
+import {
+  ContainerLoginScreen,
+  ContainerLogoScreen,
+  InputAreaLoginScreen,
+  TextLeftSide,
+  TitleLoginScreen,
+} from "../../styles/LoginScreen.styles";
+import { UserOutlined, MailOutlined, KeyOutlined } from "@ant-design/icons";
 
 const RegisterScreen = () => {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);  
+  const [email, setEmail] = useState(""); 
+
+
+  const validateForm = () => {
+    if (user.length > 3 && password.length > 3) {
+      setIsFormValid(true);
+    } else {
+      setIsFormValid(false);
+    }
+  };
+
+  const handleLoginClick = () => {
+    validateForm();
+    if (isFormValid) {
+
+      console.log("Registro Feito, Com seus dados sendo: ", {
+        user,
+        password,
+        email,
+      });
+      alert(`Registro realizado, seus dados neste registro são: Usuário: ${user}, Senha: ${password}, Email: ${email}`);
+    } else {
+      alert("Por favor, insira um nome de usuário com mais de 3 caracteres e uma senha com mais de 3 caracteres.");
+    }
+  };
+
   return (
     <ContainerLoginScreen>
       <ContainerLogoScreen>
-        <TextLeftSide>Registro de Usuário</TextLeftSide>
+        <SVGlogo width={125} height={125} />
+        <TextLeftSide>Registro de Testes, Crie seu usuário!</TextLeftSide>
       </ContainerLogoScreen>
-        <InputAreaLoginScreen>
-        <TitleLoginScreen>Registre-se</TitleLoginScreen>
-        </InputAreaLoginScreen>
+      <InputAreaLoginScreen>
+        <TitleLoginScreen>Registro</TitleLoginScreen>
+        <InputComponent
+          type="text"
+          placeholder="Usuário"
+          icon={<UserOutlined />}
+          onUserChange={setUser}
+        />
+        <InputComponent
+          type="password"
+          placeholder="Senha"
+          icon={<KeyOutlined />}
+          onPasswordChange={setPassword} 
+        />
+        <InputComponent
+          type="email"
+          placeholder="Email"
+          icon={<MailOutlined />}
+          onPasswordChange={setEmail} 
+        />
+        <ButtonComponent onClick={handleLoginClick}>Registrar</ButtonComponent>
+      </InputAreaLoginScreen>
     </ContainerLoginScreen>
-  )
-}
+  );
+};
 
-export default RegisterScreen
+export default RegisterScreen;
